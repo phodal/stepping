b1cac13c694804c95ba14d630a09f9be8cf8b036
Fengda HUANG <h@phodal.com>
2017-03-20 23:47:00 +0800

    [Demo] try to import

--- examples/browser/src/test.ts
+++ examples/browser/src/test.ts
@@ -1,18 +1,7 @@
-// Note: we're not using the double method, so it should be excluded from the bundle
-import { power } from 'typescript-starter'
-
-let output = ''
+import { EventPublisher } from 'eventstorming'
 
 function log (str: string) {
   console.log(str)
-  output += str + '\n'
 }
 
-log('Output:')
-
-if (power(3,4) === 81) {
-  log('✔ power(3,4) === 81')
-} else {
-  log('The "power" method seems to be broken.')
-}
-
+log('Output:');
