d24265f28be64fb1ec167406c5bde50a753e26c5
Fengda HUANG <h@phodal.com>
2017-03-20 23:13:33 +0800

    [TK] thinking in model

--- src/model/BaseModel.js
+++ src/model/BaseModel.js
@@ -1,3 +1,31 @@
-class BaseModel {
+interface Model {
+    create: function,
+    read: function,
+    update: function,
+    delete: function,
+}
 
+export default class BaseModel implements Model {
+
+    constructor() {
+
+    }
+
+    model: any = [];
+
+    create(model) {
+
+    }
+
+    update(id) {
+
+    }
+
+    read(id) {
+
+    }
+
+    delete(id) {
+
+    }
 }
