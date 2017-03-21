1c5f9aeef4fb9af24fafa75d1aa24e24879ef385
Phodal Huang <h@phodal.com>
2017-03-21 08:49:31 +0800

    fix browser issue

--- examples/browser/src/test.ts
+++ examples/browser/src/test.ts
@@ -1,7 +1,16 @@
-import { EventPublisher } from 'eventstorming'
+import { EventPublisher, EventModel } from 'eventstorming'
 
 function log (str: string) {
   console.log(str)
 }
 
 log('Output:');
+
+let eventModel = new EventModel;
+let event = eventModel.create({
+  name: "event should be created"
+});
+
+let eventRead = eventModel.read(event['id']);
+
+console.log(eventRead)
