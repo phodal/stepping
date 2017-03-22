5747f7fb0c1d8b5347e216e986e0d5cd33b472ac
Fengda HUANG <h@phodal.com>
2017-03-22 22:40:40 +0800

    [graphics] create absic graphic

--- src/utils/Renderer.ts
+++ src/utils/Renderer.ts
@@ -2,7 +2,7 @@ import {EventEntity} from "../entity/EventEntity";
 export default class Renderer {
 
   createEntity(node: EventEntity, nodes: EventEntity) {
-    
+
   }
 
   updateEntity(node: EventEntity, nodes: EventEntity) {
