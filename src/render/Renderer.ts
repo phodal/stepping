a26e9387672052f3dcac0ce552451e2d34e84bd3
Fengda HUANG <h@phodal.com>
2017-03-23 07:46:37 +0800

    [IPos] create interfact

--- src/render/Renderer.ts
+++ src/render/Renderer.ts
@@ -1,11 +1,20 @@
 import {EventEntity} from "../entity/EventEntity";
 export default class Renderer {
-
   createEntity(node: EventEntity, nodes: EventEntity[]) {
 
+    this.calculatePositions(nodes);
   }
 
   updateEntity(node: EventEntity, nodes: EventEntity[]) {
+
+    this.calculatePositions(nodes);
+  }
+
+  calculatePositions(nodes: EventEntity[]) {
+
+  }
+  
+  build() {
 
   }
 }
--- src/render/SVGGenerator.ts
+++ src/render/SVGGenerator.ts
@@ -1,10 +1,12 @@
+import {IPosition} from "./IPosition";
+
 export class SVGGenerator {
   buildBody(node): string {
     return `<svg width="1024" height="1024" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"> ${node} </svg>`
 
   }
 
-  buildNode(position, text): string {
+  buildNode(position: IPosition, text: string): string {
     let pos = position;
     return `<g> \
               <rect x="${pos.x}" y="${pos.y}" width="100" height="100" rx="2" ry="2" fill="#FFCC33"/> \
@@ -12,5 +14,9 @@ export class SVGGenerator {
                 <tspan x="${pos.x + 5}" dy="0">${text}</tspan> \
               </text> \
             </g>`
+  }
+
+  buildNodes() {
+
   }
 }
