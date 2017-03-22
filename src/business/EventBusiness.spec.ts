7c6233b94d965c0686a2a8047193a7806f34bd1d
Fengda HUANG <h@phodal.com>
2017-03-22 20:03:32 +0800

    [T] dirty fix

--- src/business/EventBusiness.spec.ts
+++ src/business/EventBusiness.spec.ts
@@ -7,11 +7,11 @@ test('should enable add related child', t => {
   //
   // let eventBusiness = new EventBusiness(eventSubscriber);
   //
-  // eventBusiness.createEventSticky('事件贴纸已创建');
+  // let eventEntity = eventBusiness.createEventSticky('事件贴纸已创建');
   // eventBusiness.createEventSticky('事件贴纸位置已生成');
   // eventBusiness.createEventSticky('事件贴纸已渲染');
   //
-  // t.deepEqual(eventBusiness.eventEntities[0].name, 'hello');
+  // t.deepEqual(eventEntity[0].name, 'hello');
   //
   // t.deepEqual(eventStore.store[0]['name'], 'hello');
   t.deepEqual(true, true)
