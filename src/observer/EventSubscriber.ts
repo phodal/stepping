46e98936fa913708375553b117eab9171972ee1c
Phodal Huang <h@phodal.com>
2017-03-22 08:42:44 +0800

    [events] rethink event subscriber

--- src/observer/EventSubscriber.ts
+++ src/observer/EventSubscriber.ts
@@ -1,24 +1,33 @@
 export class EventSubscriber implements EventStorming.Observer.IObserver {
-  events: string[] = [];
+  private _events: object[] = [];
 
   constructor() {
 
   }
 
   public ReceiveNotification(eventName: string, obj: object) {
-    let hasRegisterEvent = this.events && this.events.length > 0;
+    let hasRegisterEvent = this._events && this._events.length > 0;
     if (!hasRegisterEvent) {
       return;
     }
 
-    for(let index in this.events) {
-      if(this.events[index] === eventName) {
-        console.log("Event Client [" + eventName + "], Received a message:", obj);
+    for(let index in this._events) {
+      if(this._events[index]["name"] === eventName && this._events[index]["action"]) {
+        this._events[index]["action"]();
       }
     }
   }
 
   public registerEvent(event: EventStorming.Event.IEvent) {
-    this.events.push(event.name);
+    this._events.push(event);
   }
+
+  get events(): Object[] {
+    return this._events;
+  }
+
+  set events(value: Object[]) {
+    this._events = value;
+  }
+
 }
