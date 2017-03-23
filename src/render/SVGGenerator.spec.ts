bf6d1135230683ca9020edd2e90b4a102639d5fb
Phodal Huang <h@phodal.com>
2017-03-23 08:40:16 +0800

    [generator] change interface to entity

--- src/render/SVGGenerator.spec.ts
+++ src/render/SVGGenerator.spec.ts
@@ -1,9 +1,10 @@
 import {test} from "ava";
-import {SVGGenerator} from "eventstorming";
+import {SVGGenerator, EventEntity} from "eventstorming";
 
 test('should return correctly node result', t => {
   let svgGenerator = new SVGGenerator();
-  let result = svgGenerator.buildNode({x: 50, y: 50}, 'sticker had created');
+  let eventEntity = new EventEntity('sticker had created');
+  let result = svgGenerator.buildNode({x: 50, y: 50}, eventEntity);
   t.deepEqual(result, '<g>               <rect x=\"50\" y=\"50\" width=\"100\" height=\"100\" rx=\"2\" ry=\"2\" fill=\"#FFCC33\"/>               <text x=\"50\" y=\"80\" fill=\"#000\">                 <tspan x=\"55\" dy=\"0\">sticker had created</tspan>               </text>             </g>')
 });
 
--- src/render/SVGGenerator.ts
+++ src/render/SVGGenerator.ts
@@ -1,16 +1,16 @@
 import {IPosition} from "./IPosition";
+import {EventEntity} from "../entity/EventEntity";
 
 export class SVGGenerator {
   buildBody(node): string {
     return `<svg width="1024" height="1024" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"> ${node} </svg>`
-
   }
 
-  buildNode(position: IPosition, text: string): string {
+  buildNode(position: IPosition, entity: EventEntity): string {
     return `<g> \
               <rect x="${position.x}" y="${position.y}" width="100" height="100" rx="2" ry="2" fill="#FFCC33"/> \
               <text x="${position.x}" y="${position.y + 30}" fill="#000"> \
-                <tspan x="${position.x + 5}" dy="0">${text}</tspan> \
+                <tspan x="${position.x + 5}" dy="0">${entity.name}</tspan> \
               </text> \
             </g>`
   }
