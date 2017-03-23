d27558b4f08087e7920c10114b80705769e1d58f
Fengda HUANG <h@phodal.com>
2017-03-23 22:45:16 +0800

    [render] test for two node

--- src/render/Renderer.spec.ts
+++ src/render/Renderer.spec.ts
@@ -18,6 +18,30 @@ test('should return correctly node svg', t => {
                 <tspan x=\"5\" dy=\"0\">sticker had created</tspan>
               </text>
             </g> </svg>`);
+});
 
+test('should return correctly two node', t => {
+  let svgGenerator = new SVGGenerator();
+  let renderer = new Renderer(svgGenerator);
+
+  let eventEntities: EventEntity[] = [];
+  let eventEntity = new EventEntity('sticker had created');
+
+  eventEntities.push(eventEntity);
   renderer.createEntity(eventEntity, eventEntities);
+  renderer.createEntity(eventEntity, eventEntities);
+
+  let result = renderer.render();
+  t.deepEqual(result, `<svg width=\"1024\" height=\"1024\" viewBox=\"0 0 1024 1024\" xmlns=\"http://www.w3.org/2000/svg\"> <g>
+              <rect x=\"0\" y=\"0\" width=\"100\" height=\"100\" rx=\"2\" ry=\"2\" fill=\"#FFCC33\"/>
+              <text x=\"0\" y=\"30\" fill=\"#000\">
+                <tspan x=\"5\" dy=\"0\">sticker had created</tspan>
+              </text>
+            </g><g>
+              <rect x=\"0\" y=\"0\" width=\"100\" height=\"100\" rx=\"2\" ry=\"2\" fill=\"#FFCC33\"/>
+              <text x=\"0\" y=\"30\" fill=\"#000\">
+                <tspan x=\"5\" dy=\"0\">sticker had created</tspan>
+              </text>
+            </g> </svg>`);
+
 });
