4eef2103c3197dcc4a91f0ad7233b2d6090c2af5
Phodal Huang <h@phodal.com>
2017-03-23 08:47:54 +0800

    [render] add parameter

--- src/render/SVGGenerator.ts
+++ src/render/SVGGenerator.ts
@@ -6,17 +6,13 @@ export class SVGGenerator {
     return `<svg width="1024" height="1024" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"> ${node} </svg>`
   }
 
-  buildNode(position: IPosition, entity: EventEntity): string {
+  buildNode(position: IPosition, entity: EventEntity, extend?: string): string {
     return `<g> \
               <rect x="${position.x}" y="${position.y}" width="100" height="100" rx="2" ry="2" fill="#FFCC33"/> \
               <text x="${position.x}" y="${position.y + 30}" fill="#000"> \
                 <tspan x="${position.x + 5}" dy="0">${entity.name}</tspan> \
               </text> \
             </g>`
-  }
-
-  buildChild(position: IPosition, entity: EventEntity) {
-    
   }
 
   buildNodeWithChild() {
