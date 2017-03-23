b8173f58eadb4515ac1816bd68d1c3853784d49b
Phodal Huang <h@phodal.com>
2017-03-23 11:16:32 +0800

    [render] add pos to event position entity

--- src/entity/EventPositionEntity.ts
+++ src/entity/EventPositionEntity.ts
@@ -1,12 +1,28 @@
 import {EventEntity} from "./EventEntity";
 import {IPosition} from "../render/IPosition";
+import {StringIdentity} from "./Identity";
+import {Entity} from "./Entity";
+import {modelInterface} from "../model/IModel";
 
-export class EventPositionEntity {
-  private pos: IPosition;
-  private entity: EventEntity;
 
-  constructor(pos: IPosition, entity: EventEntity) {
-    this.pos = pos;
+export interface IEventPosition extends modelInterface {
+  pos?: IPosition
+}
+
+export class EventPositionEntity extends Entity<StringIdentity> {
+  private entity: IEventPosition;
+  private subEntity: modelInterface[];
+  private nearEntity: modelInterface[];
+
+  constructor(pos: IPosition, entity: modelInterface) {
+    super(entity.id);
+
     this.entity = entity;
+    this.entity.pos = pos;
+
+    this.subEntity = entity.relatedNodes || [];
+    this.nearEntity = entity.nearNode || [];
+    delete this.entity['relatedNodes'];
+    delete this.entity['nearNode'];
   }
 }
