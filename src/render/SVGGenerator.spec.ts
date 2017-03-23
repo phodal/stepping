975b0f0f6a03a3347c3a19ec4a83affb245cfb58
Phodal Huang <h@phodal.com>
2017-03-23 10:15:06 +0800

    [render] handle for child node change

--- src/render/SVGGenerator.spec.ts
+++ src/render/SVGGenerator.spec.ts
@@ -13,16 +13,28 @@ test('should return correctly node svg', t => {
             </g>`)
 });
 
-test('should return correctly node svg with extend var', t => {
+test('should return correctly node svg with extend content', t => {
   let svgGenerator = new SVGGenerator();
   let eventEntity = new EventEntity('sticker had created');
-  let result = svgGenerator.buildNode({x: 50, y: 50}, eventEntity, '<g></g>', {color: '#0095DD'});
+  let result = svgGenerator.buildNode({x: 50, y: 50}, eventEntity, {level: 'same', content: '<g></g>'}, {color: '#0095DD'});
   t.deepEqual(result, `<g>
               <rect x=\"50\" y=\"50\" width=\"100\" height=\"100\" rx=\"2\" ry=\"2\" fill=\"#0095DD\"/>
               <text x=\"50\" y=\"80\" fill=\"#000\">
                 <tspan x=\"55\" dy=\"0\">sticker had created</tspan>
               </text><g></g>
             </g>`)
+});
+
+test('should return correctly node svg with child var', t => {
+  let svgGenerator = new SVGGenerator();
+  let eventEntity = new EventEntity('sticker had created');
+  let result = svgGenerator.buildNode({x: 50, y: 50}, eventEntity, {level: 'child', content: '<g></g>'}, {color: '#0095DD'});
+  t.deepEqual(result, `<g>
+              <rect x=\"50\" y=\"50\" width=\"100\" height=\"100\" rx=\"2\" ry=\"2\" fill=\"#0095DD\"/>
+              <text x=\"50\" y=\"80\" fill=\"#000\">
+                <tspan x=\"55\" dy=\"0\">sticker had created</tspan>
+              </text>
+            </g><g></g>`)
 });
 
 test('should return correctly node header', t => {
--- src/render/SVGGenerator.ts
+++ src/render/SVGGenerator.ts
@@ -6,24 +6,32 @@ export class SVGGenerator {
     return `<svg width="1024" height="1024" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"> ${node} </svg>`
   }
 
-  buildNode(position: IPosition, entity: EventEntity, extend?: string, options?: object): string {
+  buildNode(position: IPosition, entity: EventEntity, extend?: object, options?: object): string {
     let extendField = '';
-    if(extend) {
-      extendField = extend;
-    }
+    let sameLevelField = '';
     let color='#FFCC33';
+
+    if(extend && extend['level'] === 'same') {
+      extendField = extend['content'];
+    }
+
+    if(extend && extend['level'] === 'child') {
+      sameLevelField = extend['content'];
+    }
+
     if(options && options['color']) {
       color = options['color'];
     }
+
     return `<g>
               <rect x="${position.x}" y="${position.y}" width="100" height="100" rx="2" ry="2" fill="${color}"/>
               <text x="${position.x}" y="${position.y + 30}" fill="#000">
                 <tspan x="${position.x + 5}" dy="0">${entity.name}</tspan>
               </text>${extendField}
-            </g>`
+            </g>${sameLevelField}`
   }
 
-  buildNodeWithChild() {
+  buildChild() {
 
   }
 }
