626a6607e22807cde775cc6ca472513297f3f9ec
Phodal Huang <h@phodal.com>
2017-03-24 13:18:06 +0800

    [bau] thinking in update entity

--- src/business/EventBusiness.spec.ts
+++ src/business/EventBusiness.spec.ts
@@ -1,5 +1,5 @@
 import {test} from "ava";
-import {EventBusiness, EventBusinessStore, EventSubscriber, EventPublisher} from "eventstorming";
+import {EventBusiness, EventEntity, EventStickyRender, EventBusinessStore, EventSubscriber, EventPublisher} from "eventstorming";
 
 test('should enable add related child', t => {
   // let eventSubscriber = new EventSubscriber();
@@ -15,4 +15,25 @@ test('should enable add related child', t => {
   //
   // t.deepEqual(eventStore.store[0]['name'], 'hello');
   t.deepEqual(true, true)
+});
+
+test('should enable add related child', t => {
+  let eventSubscriber = new EventSubscriber();
+  let eventRender = new EventStickyRender(eventSubscriber);
+
+  let eventBusiness = new EventBusiness(eventSubscriber);
+
+  let eventEntity = eventBusiness.createEventSticky('事件贴纸已创建');
+  let subEntity = new EventEntity('sticker had store');
+  eventEntity.addRelatedChild(subEntity);
+
+  eventBusiness.updateEventSticky(eventEntity);
+  let result = eventRender.render();
+
+  t.deepEqual(result, `<svg width=\"1024\" height=\"1024\" viewBox=\"0 0 1024 1024\" xmlns=\"http://www.w3.org/2000/svg\"> <g>
+              <rect x=\"0\" y=\"0\" width=\"100\" height=\"100\" rx=\"2\" ry=\"2\" fill=\"#FFCC33\"/>
+              <text x=\"0\" y=\"30\" fill=\"#000\">
+                <tspan x=\"5\" dy=\"0\">事件贴纸已创建</tspan>
+              </text>
+            </g> </svg>`)
 });
