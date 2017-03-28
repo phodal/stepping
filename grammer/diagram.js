d52f4925d4936a9c8ce2a00d059bc84d4fc69661
Fengda HUANG <h@phodal.com>
2017-03-28 21:58:21 +0800

    [DSL] thinking in parse

--- grammer/diagram.js
+++ grammer/diagram.js
@@ -4,6 +4,7 @@ function Diagram() {
 
 Diagram.data = [];
 Diagram.currentDomain = {};
+Diagram.currentLevel = undefined;
 Diagram.lastType = {
   key: '',
   value: []
@@ -14,15 +15,16 @@ Diagram.TYPE = {
 };
 
 Diagram.TYPE_TO_LEVEL = {
-  'domain': 0,
-  'aggregate': 1,
-  'entity': 2,
-  'model': 3,
-  'event': 4,
-  'command': 5,
+  'domain': 1,
+  'aggregate': 2,
+  'entity': 3,
+  'model': 4,
+  'event': 5,
+  'command': 6,
 };
 
 Diagram.LEVEL_MAP = [
+  '',
   'domain',
   'aggregate',
   'entity',
@@ -39,6 +41,10 @@ Diagram.isSubLevel = function (type1, type2) {
   return Diagram.TYPE_TO_LEVEL[type1] < Diagram.TYPE_TO_LEVEL[type2];
 };
 
+Diagram.isSameLevel = function (type1, type2) {
+  return Diagram.TYPE_TO_LEVEL[type1] === Diagram.TYPE_TO_LEVEL[type2];
+};
+
 Diagram.getParentLevel = function (type1) {
   return Diagram.LEVEL_MAP[Diagram.TYPE_TO_LEVEL[type1] - 1];
 };
@@ -48,13 +54,33 @@ Diagram.store = function (actor, type, value) {
   items[type + ''] = value;
 
   let isSubLevel = Diagram.isSubLevel(Diagram.lastType['key'], type);
+  let isSameLevel = Diagram.isSameLevel(Diagram.lastType['key'], type);
   let parentLevel = Diagram.getParentLevel(type);
-  console.log(parentLevel);
 
-  if(isSubLevel) {
+  if(Diagram.currentLevel === undefined) {
+    
+  }
+
+  console.log(Diagram.lastType['key'], type);
+  console.log(isSubLevel, isSameLevel);
+
+  if (isSubLevel) {
+    Diagram.lastType[type + ''] = {
+      key: '',
+      value: []
+    };
+    Diagram.lastType[type + '']['value'].push(items);
+  } else if (isSameLevel) {
     Diagram.lastType['key'] = type;
     Diagram.lastType['value'].push(items);
+  } else {
+    console.log(isSubLevel);
+    console.log(isSameLevel);
+    console.log(type)
   }
+
+  Diagram.currentDomain[type + ''] = items;
+  Diagram.lastType['key'] = type;
 
   return [actor, type, value];
 };
@@ -78,11 +104,11 @@ Diagram.createDomain = function (input) {
 
   let currentDomain = {
     domain: {
-      key: input,
-      aggregate: []
+
     }
   };
 
+  Diagram.lastType['key'] = 'domain';
   Diagram.currentDomain = currentDomain;
 };
 
