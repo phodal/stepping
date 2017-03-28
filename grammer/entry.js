b024afdec19567ff387341862878fd3659b42776
Phodal Huang <h@phodal.com>
2017-03-28 13:23:12 +0800

    [T] remove entry .js

--- grammer/entry.js
+++
@@ -1,6 +0,0 @@
-var Diagram = require('./Diagram');
-var esDsl = require('./es-dsl');
-
-Diagram.parse("domain: 订单子域\r\n   - aggregate: 聚合\r\n   - entity:实体\r\n   - entity:实体");
-Diagram.getResult();
-
