c9b776edf38dcaa61c217fe480ec6018aa107bbd
Phodal Huang <h@phodal.com>
2017-03-29 12:53:04 +0800

    [T] fix rollup config issue for esdsl

--- config/exports/rollup.config.js
+++ config/exports/rollup.config.js
@@ -4,18 +4,19 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import alias from 'rollup-plugin-alias';
 
 const substituteModulePaths = {
-    'crypto': 'build/module/adapters/crypto.browser.js',
-    'hash.js': 'build/temp/hash.js'
+  'crypto': 'build/module/adapters/crypto.browser.js',
+  'hash.js': 'build/temp/hash.js'
 }
 
 export default {
-    entry: 'build/module/index.js',
-    sourceMap: true,
-    plugins: [
-        alias(substituteModulePaths),
-        nodeResolve({
-            browser: true
-        }),
-        commonjs()
-    ]
+  entry: 'build/module/index.js',
+  sourceMap: true,
+  external: ['fs'],
+  plugins: [
+    alias(substituteModulePaths),
+    nodeResolve({
+      browser: true
+    }),
+    commonjs()
+  ]
 }
