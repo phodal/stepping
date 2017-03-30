c8aa2978f65cc34dcc19d599d570990ed4524d5e
Fengda HUANG <h@phodal.com>
2017-03-31 07:35:43 +0800

    [DSL] fix test

--- src/dsl/DSLAdapter.spec.ts
+++ src/dsl/DSLAdapter.spec.ts
@@ -10,9 +10,9 @@ test('should return current domain', t => {
   t.deepEqual(JSON.stringify(result), '[{\"name\":\"订单子域\",\"type\":\"domain\",\"aggregates\":[]}]');
 });
 
-// test('should return current domain with aggregate', t => {
-//   let dsl = new DSLAdapter();
-//   let result = dsl.parseDSL(`domain:订单子域 \r\n aggregate:订单`);
-//
-//   t.deepEqual(JSON.stringify(result), '');
-// });
+test('should return current domain with aggregate', t => {
+  let dsl = new DSLAdapter();
+  let result = dsl.parseDSL('domain:订单子域 \r\n aggregate:订单');
+
+  t.deepEqual(JSON.stringify(result), '[{\"name\":\"订单子域 \",\"type\":\"domain\",\"aggregates\":[{\"name\":\"订单\",\"type\":\"aggregate\",\"events\":[],\"commands\":[]}]}]');
+});
