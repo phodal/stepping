330550917b36571ce9e476e3c43f21e536104803
Phodal Huang <h@phodal.com>
2017-03-31 08:25:13 +0800

    [entity] create AggregateEntity class

--- src/entity/AggregateEntity.ts
+++ src/entity/AggregateEntity.ts
@@ -1,3 +1,11 @@
-export class AggregateEntity {
-  
+import {Entity} from './Entity';
+import {StringIdentity} from './Identity';
+
+export class AggregateEntity extends Entity<StringIdentity> {
+  name: string;
+
+  constructor(name: any) {
+    super(name);
+    this.name = name;
+  }
 }
