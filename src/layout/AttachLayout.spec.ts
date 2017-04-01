a8391b64ced013f96aeb7c96e60850a86eb10d53
Phodal Huang <h@phodal.com>
2017-04-01 13:23:27 +0800

    [layout] Attach handle for event number

--- src/layout/AttachLayout.spec.ts
+++ src/layout/AttachLayout.spec.ts
@@ -5,8 +5,18 @@ import {
 
 test('should enable get first node position', t => {
   let layout = new AttachLayout();
-  let parentNode = { x: 0, y: 0, width: 400, height: 300};
-  let nodes = layout.calculateNodes(parentNode, [{ id: 0, name: '库存已增加'}]);
+  let parentNode = {x: 0, y: 0, width: 400, height: 300};
+  let nodes = layout.calculateNodes(parentNode, [{id: 0, name: '库存已增加'}]);
 
-  t.deepEqual(nodes, [{ id: 0, name: '库存已增加', position: { x: 280, y: 360}}])
+  t.deepEqual(nodes, [{id: 0, name: '库存已增加', position: {x: 280, y: 360}}])
+});
+
+test('should enable get two node position', t => {
+  let layout = new AttachLayout();
+  let parentNode = {x: 0, y: 0, width: 400, height: 300};
+  let nodes = layout.calculateNodes(parentNode, [{id: 0, name: '库存已增加'}, {id: 1, name: '库存已删除'}]);
+
+  t.deepEqual(nodes, [
+    {id: 0, name: '库存已增加', position: {x: 280, y: 360}},
+    {id: 1, name: '库存已删除', position: {x: 280, y: 60}}])
 });
--- src/layout/AttachLayout.ts
+++ src/layout/AttachLayout.ts
@@ -11,14 +11,28 @@ export class AttachLayout {
       y: parentNode.y + parentNode.height
     };
 
+    let rightBottomPos = {
+      x: parentNode.x + parentNode.width,
+      y: parentNode.y
+    };
+
     for (let index in nodes) {
       let originNode = nodes[index];
       let position = {
         x: 0,
         y: 0
       };
-      position.x = rightTopPos.x - rightTopPos.x * 0.3;
-      position.y = rightTopPos.y + rightTopPos.y * 0.2;
+
+      let isEven = (parseInt(index) + 1) % 2 === 0;
+
+      if (!isEven) {
+        position.x = rightTopPos.x - parentNode.width * 0.3;
+        position.y = rightTopPos.y + parentNode.height * 0.2;
+      } else {
+        position.x = rightBottomPos.x - parentNode.width * 0.3;
+        position.y = rightBottomPos.y + parentNode.height * 0.2;
+      }
+
       originNode.position = position;
 
       newNodes.push(originNode);
