2d641ac8b3f783aa7ba5c58c96b7d1583e82f424
Phodal Huang <h@phodal.com>
2017-04-05 11:19:17 +0800

    [entity] thinking in add position interface

--- src/entity/DomainEntity.ts
+++ src/entity/DomainEntity.ts
@@ -1,11 +1,21 @@
 import {Entity} from './Entity';
 import {StringIdentity} from './Identity';
+import {IPosition} from '../render/interface/IPosition';
 
 export class DomainEntity extends Entity<StringIdentity> {
+  private _position: IPosition;
   name: string;
 
   constructor(name: any) {
     super(name);
     this.name = name;
+  }
+
+  get position(): IPosition {
+    return this._position;
+  }
+
+  set position(value: IPosition) {
+    this._position = value;
   }
 }
