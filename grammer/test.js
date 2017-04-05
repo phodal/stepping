4fc365691ed41427c99568410ab45a41544e50b0
Fengda HUANG <h@phodal.com>
2017-04-05 22:03:55 +0800

    [grammer] add test script

---
+++ grammer/test.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+var Parser = require("jison").Parser;
+
+var diagram = require("./diagram");
+var bnf = fs.readFileSync(__dirname + '/es-dsl.jison', 'utf8');
+
+var esDsl = {
+  parse: function (input) {
+    var parser = new Parser(bnf);
+    parser.yy = diagram.Diagram;
+
+    parser.yy.currentDomain = [];
+    parser.yy.data = [];
+
+    var result = parser.parse(input);
+    console.log(result);
+    return parse;
+  }
+};
+
+var fs = require('fs');
+var path = require('path');
+var raw = fs.readFileSync(path.normalize('./phodal.ddd'), 'utf8');
+
+esDsl.parse(raw);
+
