e06127a0c490e2522d670416b1226d3f7e51d013
Phodal Huang <h@phodal.com>
2017-04-05 08:58:46 +0800

    [layout] fix attach test

--- src/layout/AttachLayout.spec.ts
+++ src/layout/AttachLayout.spec.ts
@@ -44,21 +44,21 @@ test('should enable four three node position', t => {
     {id: 3, name: '库存已修改2', position: {x: 460 , y: 60}}])
 });
 
-// test('should enable to render nodes', t => {
-//   let layout = new AttachLayout();
-//   let parentNode = {x: 0, y: 0, width: 400, height: 300};
-//   let nodes = layout.calculateNodes(parentNode, [{id: 0, name: '库存已增加'}, {id: 1, name: '库存已删除'}]);
-//   let results = layout.draw(nodes);
-//
-//   t.deepEqual(results, `<g>
-//               <rect x=\"280\" y=\"360\" width=\"100\" height=\"100\" rx=\"2\" ry=\"2\" fill=\"#FFCC33\"/>
-//               <text x=\"280\" y=\"360\" fill=\"#000\" text-anchor=\"middle\">
-//                 <tspan dx=\"50\" dy=\"50\">库存已增加</tspan>
-//               </text>
-//             </g><g>
-//               <rect x=\"280\" y=\"60\" width=\"100\" height=\"100\" rx=\"2\" ry=\"2\" fill=\"#FFCC33\"/>
-//               <text x=\"280\" y=\"60\" fill=\"#000\" text-anchor=\"middle\">
-//                 <tspan dx=\"50\" dy=\"50\">库存已删除</tspan>
-//               </text>
-//             </g>`)
-// });
+test('should enable to render nodes', t => {
+  let layout = new AttachLayout();
+  let parentNode = {x: 0, y: 0, width: 400, height: 300};
+  let nodes = layout.calculateNodes(parentNode, [{id: 0, name: '库存已增加'}, {id: 1, name: '库存已删除'}]);
+  let results = layout.draw(nodes);
+
+  t.deepEqual(results, `<g>
+              <rect x=\"380\" y=\"280\" width=\"100\" height=\"100\" rx=\"2\" ry=\"2\" fill=\"#FFCC33\"/>
+              <text x=\"380\" y=\"280\" fill=\"#000\" text-anchor=\"middle\">
+                <tspan dx=\"50\" dy=\"50\">库存已增加</tspan>
+              </text>
+            </g><g>
+              <rect x=\"380\" y=\"-20\" width=\"100\" height=\"100\" rx=\"2\" ry=\"2\" fill=\"#FFCC33\"/>
+              <text x=\"380\" y=\"-20\" fill=\"#000\" text-anchor=\"middle\">
+                <tspan dx=\"50\" dy=\"50\">库存已删除</tspan>
+              </text>
+            </g>`)
+});
