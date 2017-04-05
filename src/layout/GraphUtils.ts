0bae76dbdd4b5bac5bfea697e2a4664d3cb8273a
Phodal Huang <h@phodal.com>
2017-04-05 11:34:49 +0800

    [utils] remove unsued methods

--- src/layout/GraphUtils.ts
+++ src/layout/GraphUtils.ts
@@ -23,29 +23,6 @@ export let GraphUtils = {
     return result;
   },
 
-  domainChildToNode(rootNodeName, aggregate) {
-    let result = {
-      'nodes': [{}],
-      'edges': [{}]
-    };
-
-    let nodes: object[] = [];
-    let edges: object[] = [];
-
-    let rootNode = {id: 0, name: rootNodeName};
-    nodes.push(rootNode);
-
-    for (let index in aggregate) {
-      let currentNode = {id: parseInt(index) + 1, name: aggregate[index].name};
-      nodes.push(currentNode);
-      edges.push([rootNode, currentNode]);
-    }
-
-    result.nodes = nodes;
-    result.edges = edges;
-    return result;
-  },
-
   toAggregateModelNode(rootNodeName, aggregate) {
     let result = {
       'nodes': [{}],
